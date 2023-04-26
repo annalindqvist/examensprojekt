945013e95e95874b54ebf2b72704ee9a2a13e213
annalindqvist <annalindqvist_@live.se>
2023-04-26 13:33:44 +0200

    upload photo to db and folder in backend and show on frontend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import UserController from "../controllers/user.js";
 import verifyJWT from "../middlewares/auth.js";
+import multer from "multer";
+import path from 'path';
 
 const UserRouter = Router();
+
+const storage = multer.diskStorage({
+    destination: "./public/images",
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + '-' + path.extname(file.originalname))
+    }
+});
+
+const upload = multer({storage: storage, limits: {fileSize: 3000000}});
 
 // SIGN IN ROUTE
 UserRouter.post("/sign-in", UserController.signIn);
@@ -12,6 +23,9 @@ UserRouter.post("/sign-up", UserController.signUpUser);
 
 // user profile, verifyJWT should look for a jwt token?
 UserRouter.get("/user/info", verifyJWT, UserController.getUserInfo);
+
+// user profile, verifyJWT should look for a jwt token?
+UserRouter.put("/user/edit", verifyJWT, upload.single('image'), UserController.editProfile);
 
 
 export default UserRouter;
