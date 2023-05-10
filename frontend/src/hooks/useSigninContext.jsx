b170f7c82dfd85e757f23a57a5bfc9ceea44d9d2
annalindqvist <annalindqvist_@live.se>
2023-05-10 09:10:00 +0200

    route testing agin

--- frontend/src/hooks/useSigninContext.jsx
+++ frontend/src/hooks/useSigninContext.jsx
@@ -10,7 +10,7 @@ export const useSignin = () => {
     setIsLoading(true)
     setError(null)
 
-    const res = await fetch('http://localhost:8080/backend/sign-in', {
+    const res = await fetch('http://143-42-49-241.ip.linodeusercontent.com/backend/sign-in', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
